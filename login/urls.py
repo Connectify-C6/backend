f831358bf229a49bcb87ed85f17afd6c78767914
iqbalpa <iqbalpahlevi16@gmail.com>
2023-11-28 19:43:48 +0700

    feat: implement register, login, logout

--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,7 @@ app_name = 'login'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url='/auth/login')
 def index(request):
     return JsonResponse({"Message":"Hello World"})
+
+@csrf_exempt
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid(): 
+            user = form.save()
+            user_profile = UserProfile(
+                user=user, 
+                role="common_user",
+                bio="",
+                count_reported=0
+            )
+            user_profile.save()
+            return redirect('login:login_user')
+    else:
+        form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+@csrf_exempt
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(
+            request, 
+            username=username, 
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            return redirect("login:index")
+    else:
+        return render(request, "login.html")
+
+@csrf_exempt
+def logout_user(request):
+    logout(request) 
+    return redirect("login:login_user")
