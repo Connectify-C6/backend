90c16ed624db5ac7b402fed6125e2776c400bfaa
Muhammad Rifqi Adli Gumay <88142116+asta-kunn@users.noreply.github.com>
2023-11-30 11:46:15 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '', # sesuaikan dengan password postgresql anda
+        'PASSWORD': 'lulusrpl100%', # sesuaikan dengan password postgresql anda
         'HOST': 'db.nlopyspyltpbobrvazbs.supabase.co',
         'PORT': '5432',     # sesuaikan dengan port postgresql anda   
     }
