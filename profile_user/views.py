20473d86acc696aeaa45b2828d0f4a8c7f95851a
Ikhlasul Akmal Hanif <2112akmal@gmail.com>
2023-12-11 14:45:12 +0700

    report FE done

--- profile_user/views.py
+++ profile_user/views.py
@@ -2,8 +2,9 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from login.models import UserProfile
 from django.contrib.auth.models import User
+from report.models import Report
 import json
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
     return JsonResponse({"Message":"Hello this is from profile app"})
@@ -42,16 +43,31 @@ def update_profile(request, username):
     else:
         return redirect('profile_user:show_form_update_profile')
 
+
 def get_profile_by_username(request, username):
+    # Fetch the requested user's profile
     print(request.user)
-    print(request.user.is_authenticated)
-    user = UserProfile.objects.get(user__username=username)
-    print(user.role)
+    
+    if not request.user.is_authenticated:
+        return redirect('login:login_user')
+    
+    user = get_object_or_404(UserProfile, user__username=username)
+
+    print(user.get_data()['username'])
+    # Fetch the logged-in user
+    logged_in_user = request.user
+    
+    # Check if the logged-in user has already reported this profile
+    already_reported = Report.objects.filter(reported_user=user.user, reporter=logged_in_user).exists()
+
     context = {
-        "this_user" : user,
-        "username" : request.user,
+        "this_user": user.get_data(),
+        "username": user.get_data()['username'],
         "role": user.role,
         "bio": user.bio,
+        "already_reported": already_reported
     }
-    print(context["username"])
+
+    print(user.get_data())
+
     return render(request, 'profile.html', context)
